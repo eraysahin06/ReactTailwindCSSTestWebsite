286986ffbe0eab75f1b7b041ca05c1028c3fa560
Eray Sahin <eraysahinmoon@gmail.com>
2023-07-23 21:44:46 -0400

    added Get Started Button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,6 +12,7 @@ const Hero = () => {
                 <Typed className='md:text-5xl sm:text-4xl text-xl font-bold pl-2' strings={['BTB', 'BTC', 'SaaS']} typeSpeed={120} backSpeed={140} loop/>
             </div>
             <p className='md:text-2xl text-xl font-bold text-gray-500 md:pl-4'>Monitor your data analytics to increase revenue for BTB, BTC, & SaaS platforms.</p>
+            <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
         </div>
     </div>
   )
